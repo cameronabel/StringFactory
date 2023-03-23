3f9b35d9c75877d5567bcbfaf8d4736e6deab029
Cameron Abel <cameronabel@gmail.com>
2023-03-22 22:07:35 -0700

    basic viewable homepage

---
+++ Factory/Models/FactoryContext.cs
@@ -0,0 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Factory.Models;
+
+public class FactoryContext : DbContext
+{
+  public FactoryContext(DbContextOptions options) : base(options) { }
+}
