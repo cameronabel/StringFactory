f541f3afcbefe62b5fa8ec735c96d552a7452b83
Cameron Abel <cameronabel@gmail.com>
2023-03-26 02:34:48 -0700

    machine edits

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -36,4 +36,33 @@ public class MachinesController : Controller
     _db.SaveChanges();
     return RedirectToAction("Index", "Home");
   }
+
+  public ActionResult Edit(int id)
+  {
+    Machine thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+    return View(thisMachine);
+  }
+
+  [HttpPost]
+  public ActionResult Edit(Machine machine)
+  {
+    _db.Machines.Update(machine);
+    _db.SaveChanges();
+    return RedirectToAction("Index", "Home");
+  }
+
+  public ActionResult Delete(int id)
+  {
+    Machine thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+    return View(thisMachine);
+  }
+
+  [HttpPost, ActionName("Delete")]
+  public ActionResult DeleteConfirmed(int id)
+  {
+    Machine thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+    _db.Machines.Remove(thisMachine);
+    _db.SaveChanges();
+    return RedirectToAction("Index", "Home");
+  }
 }
